8c3812d5393e288950855d547b2dee82a539a17d
Hayo <hamadayouta@hamada-no-MacBook-Air.local>
2013-01-27 12:19:00 +0900

    remove all test definition

--- GLES/GLView.m
+++ GLES/GLView.m
@@ -19,21 +19,11 @@
 
 #define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
-#define SH 1
-
 @implementation GLView
 
 CAEAGLLayer* _eaglLayer;
 EAGLContext* _context;
 GLuint _colorRenderBuffer;
-
-#if SH
-#else
-GLuint _positionSlot;
-GLuint _colorSlot;
-GLuint _projectionUniform;
-GLuint _modelViewUniform;
-#endif
 
 float _currentRotation;
 GLuint _depthRenderBuffer;
@@ -81,11 +71,7 @@ const GLubyte Indices[] = {
         // initialize
         [self setupLayer];
         [self setupContext];
-#if SH
         [Shader compileShaders];
-#else
-        [self compileShaders];
-#endif
         [self setupDepthBuffer];
         [self setupRenderBuffer];
         [self setupFrameBuffer];
@@ -181,11 +167,7 @@ float rotateZ = 0;
     // projection matrix
     float aspect = fabsf(self.frame.size.width / self.frame.size.height);
     GLKMatrix4 projection = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0f), aspect, 0.1f, 100.0f);
-#if SH
     glUniformMatrix4fv([Shader projectionMatrixSlot], 1, 0, projection.m);
-#else
-    glUniformMatrix4fv(_projectionUniform, 1, 0, projection.m);
-#endif
     
     // modelview matrix
     // - transform
@@ -198,104 +180,16 @@ float rotateZ = 0;
     rotateZ += 0.3;
     modelView = GLKMatrix4Rotate(modelView, rotateX, rotateY, rotateZ, 1);
     
-#if SH
     glUniformMatrix4fv([Shader modelViewMatrixSlot], 1, 0, modelView.m);
-#else
-    glUniformMatrix4fv(_modelViewUniform, 1, 0, modelView.m);
-#endif
     
     // 1
     glViewport(0, 0, self.frame.size.width, self.frame.size.height);
     
     // 2
-#if SH
     [mesh draw:[Shader positionSlot] colorHandle:[Shader colorSlot]];
-#else
-    [mesh draw:_positionSlot colorHandle:_colorSlot];
-#endif
     
     [_context presentRenderbuffer:GL_RENDERBUFFER];
 }
-
-#pragma mark compile shaders
-#if SH
-#else
-- (GLuint)compileShader:(NSString*)shaderName withType:(GLenum)shaderType {
-    
-    // 1
-    NSString* shaderPath = [[NSBundle mainBundle] pathForResource:shaderName
-                                                           ofType:@"glsl"];
-    NSError* error;
-    NSString* shaderString = [NSString stringWithContentsOfFile:shaderPath
-                                                       encoding:NSUTF8StringEncoding error:&error];
-    if (!shaderString) {
-        NSLog(@"Error loading shader: %@", error.localizedDescription);
-        exit(1);
-    }
-    
-    // 2
-    GLuint shaderHandle = glCreateShader(shaderType);
-    
-    // 3
-    const char * shaderStringUTF8 = [shaderString UTF8String];
-    int shaderStringLength = [shaderString length];
-    glShaderSource(shaderHandle, 1, &shaderStringUTF8, &shaderStringLength);
-    
-    // 4
-    glCompileShader(shaderHandle);
-    
-    // 5
-    GLint compileSuccess;
-    glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &compileSuccess);
-    if (compileSuccess == GL_FALSE) {
-        GLchar messages[256];
-        glGetShaderInfoLog(shaderHandle, sizeof(messages), 0, &messages[0]);
-        NSString *messageString = [NSString stringWithUTF8String:messages];
-        NSLog(@"%@", messageString);
-        exit(1);
-    }
-    
-    return shaderHandle;
-    
-}
-
-- (void)compileShaders {
-    
-    // 1
-    GLuint vertexShader = [self compileShader:@"vertex"
-                                     withType:GL_VERTEX_SHADER];
-    GLuint fragmentShader = [self compileShader:@"fragment"
-                                       withType:GL_FRAGMENT_SHADER];
-    
-    // 2
-    GLuint programHandle = glCreateProgram();
-    glAttachShader(programHandle, vertexShader);
-    glAttachShader(programHandle, fragmentShader);
-    glLinkProgram(programHandle);
-    
-    // 3
-    GLint linkSuccess;
-    glGetProgramiv(programHandle, GL_LINK_STATUS, &linkSuccess);
-    if (linkSuccess == GL_FALSE) {
-        GLchar messages[256];
-        glGetProgramInfoLog(programHandle, sizeof(messages), 0, &messages[0]);
-        NSString *messageString = [NSString stringWithUTF8String:messages];
-        NSLog(@"%@", messageString);
-        exit(1);
-    }
-    
-    // 4
-    glUseProgram(programHandle);
-    
-    // 5
-    _positionSlot = glGetAttribLocation(programHandle, "Position");
-    _colorSlot = glGetAttribLocation(programHandle, "SourceColor");
-    
-    _projectionUniform = glGetUniformLocation(programHandle, "Projection");
-    _modelViewUniform = glGetUniformLocation(programHandle, "Modelview");
-    
-}
-#endif // SH
 
 #pragma mark buffer objects
 
